30c3d8c86f2bd5f78453b0613c7fcf1f1befc03c
Erbol <erbol.shermamatov7@gmail.com>
2026-03-19 23:57:36 +0100

    update header download button and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -17,23 +17,23 @@ function App() {
         <Benefits />
         <Savings />
         <Catalog />
-        <Cta 
+        <Cta
           variant="green"
           title="Нужна подробная техническая информация?"
           text="Скачайте нашу полную презентацию. Внутри: схемы работы скрубберов очистки, точные расчеты КПД и сравнение эффективности с электрическими и газовыми котлами."
           buttonText="Скачать презентацию (PDF)"
-          buttonLink="/presentation.pdf"
+          buttonLink="/public/presentation.pdf"
           downloadText="Презентация_ЭКО-МЕШ.pdf"
           isDownload={true}
         />
         <Production />
         <Contacts />
-        <Cta 
+        <Cta
           variant="green"
           title="Остались вопросы по окупаемости?"
           text="В нашей полной брошюре есть подробные таблицы расчета окупаемости котлов ЭКО-МЕШ для разных типов бизнеса (клиник, заводов, ЖК).."
           buttonText="Скачать брошюру (PDF)"
-          buttonLink="/brochure.pdf"
+          buttonLink="/public/brochure.pdf"
           downloadText="Брошюра_ЭКО-МЕШ.pdf"
           isDownload={true}
         />
